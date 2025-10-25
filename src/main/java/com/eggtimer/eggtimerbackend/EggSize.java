62116005c6636ad45b8876368d36d6a140f32de5
MuhiMuhi404 <zeenatcha3255@gmail.com>
2025-10-26 02:46:39 +0700

    แก้bug ชื่อไม่ตรง

---
+++ src/main/java/com/eggtimer/eggtimerbackend/EggSize.java
@@ -0,0 +1,7 @@
+package com.eggtimer.eggtimerbackend;
+
+public enum EggSize {
+    SMALL,   // เบอร์ 3-4
+    MEDIUM,  // เบอร์ 2
+    LARGE    // เบอร์ 0-1
+}
---
+++ src/main/java/com/eggtimer/eggtimerbackend/StartTemperature.java
@@ -0,0 +1,6 @@
+package com.eggtimer.eggtimerbackend;
+
+public enum StartTemperature {
+    FRIDGE,     // แช่เย็น
+    ROOM_TEMP   // อุณหภูมิห้อง
+}
