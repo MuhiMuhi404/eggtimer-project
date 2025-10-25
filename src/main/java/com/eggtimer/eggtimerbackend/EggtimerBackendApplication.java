1da2d6104a3db0567b594e11045392e81501318c
MuhiMuhi404 <zeenatcha3255@gmail.com>
2025-10-26 02:44:55 +0700

    เปลี่ยนชื่อจาก eggtimer_backend เป็น eggtimerbackend ใน main และ test

---
+++ src/main/java/com/eggtimer/eggtimerbackend/EggtimerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eggtimer.eggtimerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggtimerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggtimerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eggtimer/eggtimerbackend/EggtimerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggtimer.eggtimerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggtimerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
