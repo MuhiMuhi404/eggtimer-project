8de16386428872075474a6bb19a5e0c31b781964
MuhiMuhi404 <zeenatcha3255@gmail.com>
2025-11-28 16:23:24 +0700

    last updated

--- src/main/java/com/eggtimer/eggtimerbackend/DuckEgg.java
+++ src/main/java/com/eggtimer/eggtimerbackend/DuckEgg.java
@@ -6,7 +6,7 @@ public class DuckEgg extends Egg {
     // Constructor ของ DuckEgg
     public DuckEgg(Doneness doneness, EggSize size, StartTemperature startTemp) {
         
-        // เรียก "super" (Constructor ของคลาสแม่) เพื่อส่งต่อค่าทั้ง 3 นี้กลับไปให้ abstract class Egg
+        // เพื่อส่งต่อค่าทั้ง 3 นี้กลับไปให้ abstract class Egg
         super(doneness, size, startTemp);
     }
     
--- src/main/java/com/eggtimer/eggtimerbackend/Egg.java
+++ src/main/java/com/eggtimer/eggtimerbackend/Egg.java
@@ -20,24 +20,23 @@ public abstract class Egg {
     }
 
     // --- Getters ---
-    // (Encapsulation) อนุญาตให้ภายนอกอ่าน ค่าได้เท่านั้น
     public Doneness getDoneness() { return this.doneness; }
     public EggSize getSize() { return this.size; }
     public StartTemperature getStartTemp() { return this.startTemp; }
 
     // --- Strategy Pattern Methods ---
     // Methodให้ Backend (Controller ที่สร้างทีหลัง) 
-    // เรียกใช้เพื่อสลับวิธีการทำ (เช่น จาก "ต้ม" เป็น "นึ่ง")
+    // เรียกใช้เพื่อสลับวิธีการทำ
     public void setCookingStrategy(CookingStrategy strategy) {
         this.strategy = strategy;
     }
 
     // Methodหลักที่ Controller จะเรียกใช้
-    // มันจะส่งต่อ หน้าที่การคำนวณไปให้ strategy ที่มันถืออยู่
+    // จะส่งต่อหน้าที่การคำนวณไปให้ strategy ที่มันถืออยู่
     public int getCookingTimeInSeconds() {
         if (this.strategy == null) {
             // ถ้าไม่มีใครตั้งค่า strategy มาให้
-            // เราจะตั้งค่า default strategy (ต้มปกติ) ให้เอง
+            // ตั้งค่าเป็น default strategy(ต้มปกติ)ให้เอง
             this.strategy = new StandardBoilingStrategy(); 
         }
         
